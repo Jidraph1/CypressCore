733289cfb3ace5b8fc0d5b771446104e0f697400
Jidraph1 <jidraphkimachia1@gmail.com>
2023-12-19 15:03:46 +0300

    authentication cypress

---
+++ cypress/e2e/auth.cy.js
@@ -0,0 +1,37 @@
+/// <reference types="Cypress" />
+
+
+describe('Authentication', () =>{
+    beforeEach(() =>{
+        cy.task('seedDatabase')
+      })
+it('should register a user', ()=>{
+    cy.visit('/signup')
+    cy.get('[data-cy="auth-email"]').click()
+    cy.get('[data-cy="auth-email"]').type('testingemail@example.com')
+    cy.get('[data-cy="auth-password"]').type('testpassword')
+    cy.get('[data-cy="auth-submit"]').click()
+    cy.location('pathname').should('eq', "/takeaways");
+    cy.getCookie('__session').its('value').should('not.be.empty')
+})
+
+
+it('should login a user', ()=>{
+    cy.visit('/login')
+    cy.get('[data-cy="auth-email"]').click()
+    cy.get('[data-cy="auth-email"]').type('test@example.com')
+    cy.get('[data-cy="auth-password"]').type('testpassword')
+    cy.get('[data-cy="auth-submit"]').click()
+    cy.location('pathname').should('eq', "/takeaways");
+    cy.getCookie('__session').its('value').should('not.be.empty')
+})
+
+it('should logout a user', ()=>{
+    cy.login()
+
+    cy.contains('Logout').click()
+    cy.location('pathname').should('eq', '/');
+    cy.getCookie('__session').its('value').should('be.empty')
+
+})
+})
--- cypress/e2e/takeaways.cy.js
+++ cypress/e2e/takeaways.cy.js
@@ -1,7 +1,28 @@
 /// <reference types="Cypress" />
 
 describe('Takeaways', () => {
+  beforeEach(() =>{
+    cy.task('seedDatabase')
+  })
   it('should display a list of fetched takeaways', () => {
     cy.visit('/')
+    cy.get('[data-cy= "takeaway-item"]').should('have.length', 2)
   });
+
+  it('should add a new takeaway', ()=>{
+    cy.intercept('POST', '/takeaways/new*', 'success').as('createTakeaways')
+    cy.login()
+    cy.contains('Add a new takeaway').click()
+    cy.visit('/takeaways/new')
+    cy.get('[data-cy="title"]').click()
+
+    cy.get('[data-cy="title"]').type("Newtitle")
+    cy.get('[data-cy="body"]').type("NewBody")
+    cy.contains('Create').click()
+
+    cy.wait('@createTakeaways')
+    .its('request.body')
+    .should('match', /Newtitle.*NewBody/)
+
+  })
 });
